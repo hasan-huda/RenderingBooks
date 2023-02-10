77ae58bb1097f17d52b352ce2c69b8c1f88820f9
Hasan Huda <hasanhuda037@gmail.com>
2023-02-10 01:53:53 -0500

    First commit

---
+++ src/test/java/com/hasan/rendering/RenderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasan.rendering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
